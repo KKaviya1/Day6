6465051dc8c656432bb1cfd330c7fee0866c735f
KKaviya1 <kaviyagandhi136@gmail.com>
2023-07-17 09:22:22 +0530

    my commit

---
+++ collection/collection/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //STACK = last in first out
+            Stack stack = new Stack();
+            stack.Push("viya");
+            stack.Push("kavi");
+            stack.Push("gai-3");
+            Console.WriteLine("number of items in stack are: \t" + stack.Count);
+          /*  foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }*/
+            Console.ReadKey();
+        }
+    }
+}
